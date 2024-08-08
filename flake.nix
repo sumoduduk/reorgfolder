a02df3815d0f9657e202c7a2f3a249f25052e4d3
sumoduduk <sumoduduk13@gmail.com>
2024-08-08 10:41:38 +0700

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "Flake file for Reorgfolder App";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    crane,
+    flake-utils,
+    ...
+  }: 
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      craneLib = crane.mkLib pkgs;
+
+      commonArgs = {
+        src = craneLib.cleanCargoSource ./.;
+        strictDeps = true;
+
+        buildInputs = [
+          #pkgs.openssl
+        ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.libiconv
+        ];
+        nativeBuildInputs = [
+          # pkgs.pkg-config
+        ];
+      };
+
+      reorgfolder = craneLib.buildPackage (commonArgs // {
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs
+          # Additional environment variables or build phases/hooks can be set
+          # here *without* rebuilding all dependency crates
+          # MY_CUSTOM_VAR = "some value";
+    });
+      in {
+      checks = {
+
+        inherit reorgfolder;
+      };
+
+      packages.default = reorgfolder;
+      
+      apps.default = flake-utils.lib.mkApp {
+        drv = reorgfolder;
+      };
+
+      devShells.default = craneLib.devShell {
+        checks = self.checks.${system};
+
+        # Additional dev-shell environment variables can be set directly
+        # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+        shellHook = ''
+          alias cls="clear"
+
+        '';
+
+
+        # Extra inputs can be added here; cargo and rustc are provided by default.
+        packages = [
+          # pkgs.ripgrep
+        ];
+      };
+
+    
+
+    });
+}
