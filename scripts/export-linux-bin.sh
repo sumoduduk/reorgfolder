c19de56868304e6347fe2e9fd2294de7867bf494
sumoduduk <sumoduduk13@gmail.com>
2024-08-09 17:33:37 +0700

    fix: cp before patch into another folder

--- scripts/export-linux-bin.sh
+++ scripts/export-linux-bin.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 name=$1
-chmod 777 "$name"
-patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 --set-rpath /lib/x86_64-linux-gnu "$name"
+out=$2
+mkdir -p "$out"
+cp "./result/bin/$name" "$out/$name"
+chmod 777 "$out/$name"
+patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 --set-rpath /lib/x86_64-linux-gnu "$out/$name"
