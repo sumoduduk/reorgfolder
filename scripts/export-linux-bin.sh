a9d55bd7de4ad3509f1f375ec6c5c81f3e3418ff
sumoduduk <sumoduduk13@gmail.com>
2024-08-10 15:05:36 +0700

    chores: rename the file too reorgfolder-x86_64-linux

--- scripts/export-linux-bin.sh
+++ scripts/export-linux-bin.sh
@@ -3,6 +3,6 @@
 name=$1
 out=$2
 mkdir -p "$out"
-cp "./result/bin/$name" "$out/$name"
-chmod 777 "$out/$name"
-patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 --set-rpath /lib/x86_64-linux-gnu "$out/$name"
+cp "./result/bin/$name" "$out/$name-x86_64-linux"
+chmod 777 "$out/$name-x86_64-linux"
+patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 --set-rpath /lib/x86_64-linux-gnu "$out/$name-x86_64-linux"
